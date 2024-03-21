fd90297c9b035f9d74f745e1f3c732ceb9cae9eb
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-03-21 23:34:33 +0200

    incidents page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import Profile from "./Pages/Profile/Profile";
 import Container from "@mui/material/Container";
+import Incidents from "./Pages/Incidents";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Container>
         <LoginPage />
         <Profile />
+        <Incidents />
       </Container>
     </div>
   );
